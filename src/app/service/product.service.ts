62a55c70300bcf422b87943b9b5a4326097e9b7b
Vojo <mladisumar@gmail.com>
2018-12-23 19:03:51 +0100

    add service

--- src/app/service/product.service.ts
+++ src/app/service/product.service.ts
@@ -1,9 +1,48 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
+import { Product } from '../model/Product';
+import { Observable } from 'rxjs/Observable';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ProductService {
+export class ProductService implements OnInit {
 
-  constructor() { }
+  private baseUrl: string = 'http://localhost:8090/discont.com/products/';
+  private productUrl: string = 'http://localhost:8090/discont.com/product/';
+
+  private product: Product;
+  constructor(private httpClient: HttpClient) { }
+
+  ngOnInit(): void {
+    this.getAllProduct();
+  }
+  getProductByID(id: number): Observable<Product> {
+
+    return this.httpClient.get<Product>(this.baseUrl + id);
+  }
+
+  getAllProduct(): Observable<Product[]> {
+
+    return this.httpClient.get<Product[]>(this.baseUrl)
+  }
+
+  deleteProduct(id: number) {
+    return this.httpClient.delete(this.productUrl + id)
+  }
+
+  updateProduct(product: Product) { 
+    return this.httpClient.put(this.productUrl + product.id, product)
+  }
+  addProduct(product: Product) {
+    return this.httpClient.post(this.productUrl, product)
+  }
+
+
+  setter(product: Product) {
+    this.product = product;
+  }
+  getter() {
+    return this.product;
+  }
 }
--- src/app/service/purchase.service.ts
+++ src/app/service/purchase.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import { Purchas } from '../model/Purchas';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PurchaseService {
 
-  constructor() { }
+  private baseUrl: string = 'http://localhost:8090/discont.com';
+
+  constructor(private httpClient: HttpClient) { }
+
+  allPurchasesByUser(userName: string): Observable<Purchas[]> {
+    return this.httpClient.get<Purchas[]>(this.baseUrl + '/purchases/by-user/' + userName)
+  }
+
+  peopleWhoPreviouslyPurchasedProduct(id: Number): Observable<Purchas[]> {
+
+    return this.httpClient.get<Purchas[]>(this.baseUrl + '/purchases/by-product/' + id)
+  }
+
 }
