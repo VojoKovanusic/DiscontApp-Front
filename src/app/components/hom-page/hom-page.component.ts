b8780bc74e9823881dbd00a89596647fd0d3cce1
Vojo <mladisumar@gmail.com>
2018-12-23 20:21:32 +0100

    add basic components

--- src/app/components/hom-page/hom-page.component.ts
+++ src/app/components/hom-page/hom-page.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class HomPageComponent implements OnInit {
   imgAngular="https://angular.io/assets/images/logos/angularjs/AngularJS-Shield.svg";
   imgSpring="https://cdn.freebiesupply.com/logos/large/2x/spring-3-logo-png-transparent.png";
+  imgBootstrap="https://www.ostraining.com/cdn/images/coding/bootstrap-hq.jpg"
   constructor() { }
 
   ngOnInit() {
